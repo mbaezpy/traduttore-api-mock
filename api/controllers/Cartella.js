f95f26e86723c2454a2bf7b18494951d5619e4ce
Marcos Baez <mbaezpy@gmail.com>
2017-10-09 14:47:46 +0200

    Expanded mock responses to Cartella integration

--- api/controllers/Cartella.js
+++ api/controllers/Cartella.js
@@ -7,4 +7,23 @@ module.exports.getShareable = function (req, res, next) {
   Cartella.getShareable(req.swagger.params, res, next);
 };
 
+module.exports.postEvent = function (req, res, next) {
+  Cartella.postEvent(req.swagger.params, res, next);
+};
+
+module.exports.getEvents = function (req, res, next) {
+  Cartella.getEvents(req.swagger.params, res, next);
+};
+
+module.exports.getEventById = function (req, res, next) {
+  Cartella.getEventById(req.swagger.params, res, next);
+};
+
+module.exports.postEventUpdate = function (req, res, next) {
+  Cartella.postEventUpdate(req.swagger.params, res, next);
+};
+
+module.exports.putEventUpdateById = function (req, res, next) {
+  Cartella.putEventUpdateById(req.swagger.params, res, next);
+};
 
--- api/controllers/CartellaService.js
+++ api/controllers/CartellaService.js
@@ -1,5 +1,67 @@
 'use strict';
 
+var EVENTS = [{
+  "id": "dd90f1ee-6c54-4b01-90e6-d701748f0850",
+  "eventIdCartella": "11",
+  "resident": {
+    "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+    "name": "string",
+    "role": "resident",
+    "contactInfos": [
+      {
+        "type": "email",
+        "value": "rsadmin@gmail.com"
+      }
+    ]
+  },
+  "createdOn": "2017-10-09T12:01:10Z",
+  "currentPhase": "string",
+  "updates": [
+    {
+      "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+      "status": "shared",
+      "phase": "string",
+      "createdOn": "2017-10-09T12:01:10Z",
+      "createdBy": {
+        "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+        "name": "string",
+        "role": "resident",
+        "contactInfos": [
+          {
+            "type": "email",
+            "value": "rsadmin@gmail.com"
+          }
+        ]
+      },
+      "statusChangedOn": "2017-10-09T12:01:10Z",
+      "statusChangedBy": {
+        "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+        "name": "string",
+        "role": "resident",
+        "contactInfos": [
+          {
+            "type": "email",
+            "value": "rsadmin@gmail.com"
+          }
+        ]
+      },
+      "staffComment": "string",
+      "staffInternalNote": "string",
+      "sharedFrom": "string",
+      "sharedInApp": true,
+      "sharedInCall": true,
+      "sharedInSM": true,
+      "updateData": [
+        {
+          "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+          "name": "string",
+          "value": "string"
+        }
+      ]
+    }
+  ]
+}];
+
 exports.getShareable = function (args, res, next) {
   /**
    * parameters expected in the args:
@@ -11,13 +73,13 @@ exports.getShareable = function (args, res, next) {
 
     "isShareable": true,
     "eventConfig": {
-      "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-      "name": "string",
-      "description": "string",
+      "id": "a290f1ee-6c54-4b01-90e6-d701748f0851",
+      "name": "Meals completion configuration",
+      "description": "Configuration of the meals consumption event",
       "currentSetting": {
-        "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-        "name": "string",
-        "description": "string",
+        "id": "b290f1ee-6c54-4b01-90e6-d701748f0851",
+        "name": "Default meals completion configuration",
+        "description": "Configuration of the meals consumption event",
         "startDate": "2017-10-09T08:35:40Z",
         "endDate": "2017-10-09T08:35:40Z",
         "isActive": true,
@@ -28,59 +90,54 @@ exports.getShareable = function (args, res, next) {
         "optSnooze": true,
         "optCancel": true,
         "shareInApp": true,
-        "shareInCall": true,
-        "shareInSM": true,
-        "extraParams": [
-          {
-            "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-            "name": "string",
-            "title": "string",
+        "shareInCall": false,
+        "shareInSM": false,
+        "extraParams": [{
+            "id": "c290f1ee-6c54-4b01-90e6-d701748f0851",
+            "name": "menu",
+            "title": "Menu of the day",
             "type": "InputText"
-        }
-      ]
+        }]
       }
     },
     "sendTo": [
       {
         "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
         "contact": {
-          "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-          "name": "string",
+          "id": "e290f1ee-6c54-4b01-90e6-d701748f0851",
+          "name": "Carla Sartori",
           "role": "resident",
-          "contactInfos": [
-            {
+          "contactInfos": [{
               "type": "email",
-              "value": "rsadmin@gmail.com"
-          }
-        ]
+              "value": "carla@gmail.com"
+          }]
         },
         "relationship": "dougther",
         "preference": {
           "shareWithMe": true,
           "shareInApp": true,
-          "shareInCall": true,
-          "shareInSM": true
+          "shareInCall": false,
+          "shareInSM": false
         },
         "persona": {
           "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-          "name": "string",
-          "suggestion": "string",
+          "name": "Worrisome",
+          "suggestion": "Explain the data with care, as it might affect the person",
           "expectedReactions": [
           "worry"
         ],
-          "members": [
-            {
-              "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-              "name": "string",
-              "role": "resident",
-              "contactInfos": [
-                {
-                  "type": "email",
-                  "value": "rsadmin@gmail.com"
-              }
-            ]
+          "members": [{
+          "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+          "contact": {
+            "id": "e290f1ee-6c54-4b01-90e6-d701748f0851",
+            "name": "Carla Sartori",
+            "role": "resident",
+            "contactInfos": [{
+                "type": "email",
+                "value": "carla@gmail.com"
+            }]
           }
-        ]
+         }]
         }
     }
   ]
@@ -93,3 +150,79 @@ exports.getShareable = function (args, res, next) {
   }
 
 }
+
+exports.postEvent = function (args, res, next) {
+  /**
+   * parameters expected in the args:
+  * body (EventSharing)
+  **/
+  // no response value expected for this operation
+    
+  var event = args.body;    
+  var eventId = "dd90f1ee-6c54-4b01-90e6-d701748f085" + EVENTS.length ;  
+  event.id = eventId;
+  
+  EVENTS.push(event);
+  
+  res.location('/sync/event/' + eventId);
+  
+  res.end();  
+};
+
+exports.getEvents = function (args, res, next) {
+/**
+  * parameters expected in the args:
+  * delegatedTo (UUID)
+  * optSnooze (Boolean)
+  * limit (Integer)
+  * skip (Integer)
+  **/
+  
+  var examples = {};
+  examples['application/json'] = [];
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }  
+  
+};
+
+exports.getEventById = function (args, res, next) {
+  
+  var examples = {};
+  examples['application/json'] = EVENTS[0]; 
+  
+  if (Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  } else {
+    res.end();
+  }  
+  
+};
+
+exports.postEventUpdate = function (args, res, next) {
+  /**
+   * parameters expected in the args:
+  * eventId (UUID)
+  * body (EventSharingUpdate)
+  **/
+  // no response value expected for this operation
+  res.end();  
+};
+
+exports.putEventUpdateById = function (args, res, next) {
+  /**
+   * parameters expected in the args:
+  * updateId (UUID)
+  * body (EventSharingUpdate)
+  **/
+  // no response value expected for this operation
+  res.end();  
+  
+};
+
