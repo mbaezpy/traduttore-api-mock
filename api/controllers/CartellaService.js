cf2f3101f318b6871edcb5f56f06d0b8e7c53b21
Marcos Baez <mbaezpy@gmail.com>
2017-10-09 16:20:09 +0200

    mock post calls of the Cartella integration keep state in memory

--- api/controllers/CartellaService.js
+++ api/controllers/CartellaService.js
@@ -1,65 +1,67 @@
 'use strict';
 
-var EVENTS = [{
-  "id": "dd90f1ee-6c54-4b01-90e6-d701748f0850",
-  "eventIdCartella": "11",
-  "resident": {
-    "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    "name": "string",
-    "role": "resident",
-    "contactInfos": [
-      {
+var USERS = [
+{
+    "id": "aa90f1ee-6c54-4b01-90e6-d701748f0850",
+    "name": "Carla Sartori",
+    "role": "family",
+    "contactInfos": [{
         "type": "email",
-        "value": "rsadmin@gmail.com"
-      }
-    ]
-  },
-  "createdOn": "2017-10-09T12:01:10Z",
-  "currentPhase": "string",
-  "updates": [
+        "value": "carla@gmail.com"
+    }]
+},
+{
+    "id": "aa90f1ee-6c54-4b01-90e6-d701748f0851",
+    "name": "Enrico Fermi",
+    "role": "staff",
+    "contactInfos": [{
+        "type": "email",
+        "value": "enrico@rsa.com"
+    }]
+},
+{
+    "id": "aa90f1ee-6c54-4b01-90e6-d701748f0852",
+    "name": "Stefano Macci",
+    "role": "resident",
+    "contactInfos": [{
+        "type": "email",
+        "value": "stefano@libero.com"
+    }]
+}   
+];
+
+var EVENT_UPDATES = [
     {
-      "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+      "id": "cc90f1ee-6c54-4b01-90e6-d701748f0850",
       "status": "shared",
-      "phase": "string",
+      "phase": "closed",
       "createdOn": "2017-10-09T12:01:10Z",
-      "createdBy": {
-        "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-        "name": "string",
-        "role": "resident",
-        "contactInfos": [
-          {
-            "type": "email",
-            "value": "rsadmin@gmail.com"
-          }
-        ]
-      },
+      "createdBy": USERS[1],
       "statusChangedOn": "2017-10-09T12:01:10Z",
-      "statusChangedBy": {
-        "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-        "name": "string",
-        "role": "resident",
-        "contactInfos": [
-          {
-            "type": "email",
-            "value": "rsadmin@gmail.com"
-          }
-        ]
-      },
-      "staffComment": "string",
-      "staffInternalNote": "string",
-      "sharedFrom": "string",
+      "statusChangedBy": USERS[1],
+      "staffComment": "The resident enjoyed the meal",
+      "staffInternalNote": "No comments",
+      "sharedFrom": "Data Entering",
       "sharedInApp": true,
-      "sharedInCall": true,
-      "sharedInSM": true,
+      "sharedInCall": false,
+      "sharedInSM": false,
       "updateData": [
         {
-          "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-          "name": "string",
-          "value": "string"
+          "id": "0090f1ee-6c54-4b01-90e6-d701748f0850",
+          "name": "meal-completeness",
+          "value": "100"
         }
       ]
     }
-  ]
+]
+
+var EVENTS = [{
+  "id": "bb90f1ee-6c54-4b01-90e6-d701748f0850",
+  "eventIdCartella": 11,
+  "resident": USERS[2],
+  "createdOn": "2017-10-09T12:01:10Z",
+  "currentPhase": "closed",
+  "updates": [EVENT_UPDATES[0]]
 }];
 
 exports.getShareable = function (args, res, next) {
@@ -94,24 +96,16 @@ exports.getShareable = function (args, res, next) {
         "shareInSM": false,
         "extraParams": [{
             "id": "c290f1ee-6c54-4b01-90e6-d701748f0851",
-            "name": "menu",
-            "title": "Menu of the day",
-            "type": "InputText"
+            "name": "meal-completness",
+            "title": "Meal completness",
+            "type": "numeric"
         }]
       }
     },
     "sendTo": [
       {
         "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-        "contact": {
-          "id": "e290f1ee-6c54-4b01-90e6-d701748f0851",
-          "name": "Carla Sartori",
-          "role": "resident",
-          "contactInfos": [{
-              "type": "email",
-              "value": "carla@gmail.com"
-          }]
-        },
+        "contact": USERS[0],
         "relationship": "dougther",
         "preference": {
           "shareWithMe": true,
@@ -128,20 +122,13 @@ exports.getShareable = function (args, res, next) {
         ],
           "members": [{
           "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-          "contact": {
-            "id": "e290f1ee-6c54-4b01-90e6-d701748f0851",
-            "name": "Carla Sartori",
-            "role": "resident",
-            "contactInfos": [{
-                "type": "email",
-                "value": "carla@gmail.com"
-            }]
-          }
+          "contact": USERS[0]          
          }]
         }
     }
   ]
   };
+  
   if (Object.keys(examples).length > 0) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
@@ -158,15 +145,25 @@ exports.postEvent = function (args, res, next) {
   **/
   // no response value expected for this operation
     
-  var event = args.body;    
-  var eventId = "dd90f1ee-6c54-4b01-90e6-d701748f085" + EVENTS.length ;  
+  var event = args.body.value;   
+  var eventId = "bb90f1ee-6c54-4b01-90e6-d701748f085" + EVENTS.length ;  
+
   event.id = eventId;
+  event.createdOn = (new Date()).toISOString();
+  event.resident = USERS.find(function(user){
+    return user.id == event.resident.id;
+  });
+  event.updates = [];  
+
+  if (event.resident) {
+    EVENTS.push(event);
+    res.location('/sync/event/' + eventId);
+    res.end();  
+  } else {
+    res.status(422);
+    res.end({status: 422, error: "Invalid resident information"});
+  }
   
-  EVENTS.push(event);
-  
-  res.location('/sync/event/' + eventId);
-  
-  res.end();  
 };
 
 exports.getEvents = function (args, res, next) {
@@ -179,7 +176,7 @@ exports.getEvents = function (args, res, next) {
   **/
   
   var examples = {};
-  examples['application/json'] = [];
+  examples['application/json'] = EVENTS;
   
   if(Object.keys(examples).length > 0) {
     res.setHeader('Content-Type', 'application/json');
@@ -212,7 +209,31 @@ exports.postEventUpdate = function (args, res, next) {
   * body (EventSharingUpdate)
   **/
   // no response value expected for this operation
-  res.end();  
+  var update = args.body.value;   
+  var eventId = args.eventId.value;
+  
+  var updateId = "cc90f1ee-6c54-4b01-90e6-d701748f0850" + EVENT_UPDATES.length ;    
+
+  update.id = updateId;
+  update.createdOn = (new Date()).toISOString();
+  update.statusChangedOn = update.createdOn;
+  update.createdBy = USERS[1];
+  update.statusChangedBy = USERS[1];
+  
+  var event = EVENTS.find(function(item){
+    return item.id == eventId;
+  });
+  
+
+  if (event) {
+    event.updates.push(update);  
+    EVENT_UPDATES.push(event);
+    res.location('/sync/event_update/' + updateId);
+    res.end();  
+  } else {
+    res.status(422);
+    res.end({status: 422, error: "Invalid event information"});
+  }
 };
 
 exports.putEventUpdateById = function (args, res, next) {
