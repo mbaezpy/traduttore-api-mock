6bac00c23327ca9a500aa46f58706d0554cabad0
Marcos Baez <mbaezpy@gmail.com>
2017-10-13 15:38:55 +0200

    added cors support

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ var config = {
   appRoot: __dirname // required config
 };
 
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+    }
+    else {
+      next();
+}});
+
+
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
